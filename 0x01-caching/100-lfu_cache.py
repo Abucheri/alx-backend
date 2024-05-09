f068bd0636784f1aecd4d45ef9b12f83eb0cf956
O'Brien <abucheriobrien@gmail.com>
2024-05-09 05:37:01 +0300

    LFU Caching update_3

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 """
 Defines the LFUCache class
+This LFU caching algorithm ensures that when the cache is full and a new item
+needs to be added, it removes the least frequently used item. If there are
+multiple least frequently used items, it removes the least recently used among
+them.
 """
 
 from collections import OrderedDict
@@ -24,6 +28,16 @@ class LFUCache(BaseCaching):
     def put(self, key, item):
         """
         Adds an item to the cache using LFU algorithm.
+        It first checks if both key and item are not None.
+        If the cache is full (the number of items exceeds
+        BaseCaching.MAX_ITEMS), it finds the least frequently used key(s)
+        by finding the minimum frequency from self.keys_freqs. If there are
+        multiple keys with the same minimum frequency, it selects the one
+        that was least recently used (LRU) based on the order in
+        self.cache_data. It then deletes the least recently used key from
+        both self.cache_data and self.keys_freqs. After that, it adds the
+        new key and item to self.cache_data and sets its frequency to 1 in
+        self.keys_freqs.
 
         Args:
             key: The key of the item to be added.
@@ -48,6 +62,10 @@ class LFUCache(BaseCaching):
     def get(self, key):
         """
         Retrieves an item from the cache.
+        It checks if key is not None and exists in self.cache_data.
+        If the key exists, it increments its frequency in self.keys_freqs
+        and returns the corresponding item from self.cache_data. If the key
+        doesn't exist or is None, it returns None.
 
         Args:
             key: The key of the item to be retrieved.
