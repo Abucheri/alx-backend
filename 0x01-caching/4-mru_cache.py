ba8e8f3dafdc8b66d7a9777b7a81767ad9ca4d6b
O'Brien <abucheriobrien@gmail.com>
2024-05-09 03:24:13 +0300

    MRU Caching

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+Defines the MRUCache class
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    MRUCache class inherits from BaseCaching and implements MRU caching
+    algorithm.
+    """
+    def __init__(self):
+        """
+        Initializes the MRUCache instance.
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Adds an item to the cache using MRU algorithm.
+
+        Args:
+            key: The key of the item to be added.
+            item: The item to be added to the cache.
+
+        Returns:
+            None
+        """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                most_recently_used_key = max(self.cache_data,
+                                             key=self.cache_data.get)
+                del self.cache_data[most_recently_used_key]
+                print("DISCARD:", most_recently_used_key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Retrieves an item from the cache.
+
+        Args:
+            key: The key of the item to be retrieved.
+
+        returns:
+            The item associated with the given key, or None if the key is not
+            found or is None.
+        """
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
